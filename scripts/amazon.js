30d65f0ea24d773e6fa65489ac89aa98297eb6fb
Noble <isaacodey01@gmail.com>
2023-08-23 22:25:12 +0100

    first commit

---
+++ scripts/amazon.js
@@ -0,0 +1,242 @@
+// const products = [{
+//         image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//         name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//         rating: {
+//             stars: 4.5,
+//             count: 87
+//         },
+//         priceCents: 1090
+//     }, {
+//         image: 'images/products/intermediate-composite-basketball.jpg',
+//         name: 'Intermediate Size Basketball',
+//         rating: {
+//             stars: 4,
+//             count: 127,
+//         },
+//         priceCents: 2095
+//     },
+//     {
+//         image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//         name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//         rating: {
+//             stars: 4.5,
+//             count: 56
+//         },
+//         priceCents: 799
+
+//     },
+//     {
+//         image: 'images/products/black-2-slot-toaster.jpg',
+//         name: '2 Slot Toaster - Black',
+//         rating: {
+//             stars: 5,
+//             count: 2197
+//         },
+//         priceCents: 1899
+//     }
+// ];
+
+let productsHTML = '';
+
+products.forEach((product) => {
+    productsHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image"
+                src="${product.image}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+                <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+                <div class="product-rating-count link-primary">
+                ${product.rating.count}
+                </div>
+            </div>
+
+            <div class="product-price">
+                $${(product.priceCents / 100).toFixed(2)}
+            </div>
+
+            <div class="product-quantity-container">
+                <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+                <img src="images/icons/checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${product.id}">
+                Add to Cart
+            </button>
+        </div>
+    `;
+
+});
+
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+let savedCart = JSON.parse(localStorage.getItem('toSaveCart'))
+
+let savedQuantity = 0;
+
+
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+            
+            let matchingItem;
+
+            productQuantity = document.querySelector(`.js-quantity-selector-${productId}`)
+
+            products.forEach((product) => {
+                myQuantity.push ({
+                    productId: product.id,
+                    prodQuantity: productQuantity.value
+                })
+            })
+
+
+            // myQuantity.forEach((quantity) => {
+            //     if (quantity.productId === productId){
+            //         quantityMatch = quantity
+            //         newQuantity = Number(quantity.prodQuantity)
+            //     }
+            // })
+
+            // console.log(newQuantity)
+
+            
+            // if (quantityMatch) {
+            //     newQuantity += Number(quantityMatch.prodQuantity)
+            //     console.log('the quantity ' + newQuantity)
+            // }
+
+            // console.log(myQuantity)
+
+            let cartQuantity;
+        
+       
+        if (savedCart != null) {
+            cart = savedCart
+
+            
+
+            cartQuantity = savedQuantity;
+
+            products.forEach((product) => {
+                if(product.id === productId) {   
+    
+                    cart.forEach((item) => {
+                        if (productId === item.productId) {
+                            matchingItem = item
+                        }
+                    })
+    
+                    if (matchingItem) {
+                        matchingItem.quantity = Number(matchingItem.quantity)
+                        matchingItem.quantity += Number(productQuantity.value);
+                    } else {
+    
+                    cart.push({
+                        productId: productId,
+                        quantity: Number(productQuantity.value)
+                    });    
+                    
+                }
+           
+                
+                cart.forEach((item) => {
+                    cartQuantity += item.quantity
+                });
+                }
+            })
+            
+        }else if (savedCart = []){
+
+        cartQuantity = 0;
+
+        products.forEach((product) => {
+            if(product.id === productId) {   
+
+                cart.forEach((item) => {
+                    if (productId === item.productId) {
+                        matchingItem = item
+                    }
+                })
+
+                if (matchingItem) {
+                    matchingItem.quantity = Number(matchingItem.quantity)
+                    matchingItem.quantity += Number(productQuantity.value);
+                } else {
+
+                cart.push({
+                    productId: productId,
+                    quantity: Number(productQuantity.value)
+                });    
+                
+            }
+       
+            
+            // cart.forEach((item) => {
+            //     cartQuantity += item.quantity
+            // });
+            }
+        })
+    }
+
+            localStorage.setItem('toSaveCart', JSON.stringify(cart))
+
+            console.log(cart)
+        
+        // document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+        });
+
+        
+
+    
+    });
+
+
+
+    document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+
+        savedCart.forEach((saved)=>{
+        savedQuantity += saved.quantity
+    })
+
+        document.querySelector('.js-cart-quantity').innerHTML = savedQuantity;
+
+        })
+    })
+    
+    if (savedCart != null) {
+        document.querySelector('.js-cart-quantity').innerHTML = savedQuantity;
+    }
+
+    console.log(savedCart)
